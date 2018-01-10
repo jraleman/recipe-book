ea1d60ba05619aca71a858657bd2fe6b96e70443
jraleman <jraleman@bendiburg.com>
2018-01-09 21:14:43 -0500

    Imported some react-bootstrap components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
+import Panel from 'react-bootstrap/lib/Panel';
+import Modal from 'react-bootstrap/lib/Modal';
+import Button from 'react-bootstrap/lib/Button';
+import ButtonToolbar from 'react-bootstrap/lib/ButtonToolbar';
+import Accordion from 'react-bootstrap/lib/Accordion';
+import FormGroup from 'react-bootstrap/lib/FormGroup';
+import FormControl from 'react-bootstrap/lib/FormControl';
+import ControlLabel from 'react-bootstrap/lib/ControlLabel';
+
 import './App.css';
 
 class App extends Component {
