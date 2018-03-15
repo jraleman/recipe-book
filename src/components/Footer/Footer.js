8a5dad8592604c800f818ecec3d477a042f8c6d8
jraleman <jraleman@bendiburg.com>
2018-03-15 00:47:13 -0700

    Added footer component, removed static test site

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-6 col-sm-12 text-center">
+              <h3>About</h3>
+              <div className="footer-about">
+                { "A recipe book is one that you use daily and what we in our " }
+                { "family call 'a living book' — a book that you use all the " }
+                { "time, not just read once and discard on the shelf. " }
+                { "Recipes are by nature derivative and meant to be shared " }
+                { "- that is how they improve, are changed, how new ideas are formed." }
+              </div>
+            </div>
+            <div className="col-md-6 col-sm-12 text-center">
+              <div className="footer-author">
+                { "Made with " }
+                <i className="fa fa-heart" aria-hidden="true" />
+                { " love by " }
+                <a href="https://jraleman.com/" target="_blank">
+                  Jose Ramon
+                </a>.
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
