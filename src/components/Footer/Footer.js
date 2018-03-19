067e7729e3149c5e828bf945e16489541e6a2c77
jraleman <jraleman@bendiburg.com>
2018-03-19 00:05:29 -0700

    Refactored Footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+// Subcomponents.
+import About from './About';
+import Author from './Author';
+
+// About info.
 const aboutQuote = "A recipe book is one that you use daily and what we in our \
 family call 'a living book' — a book that you use all the time, not just read \
 once and discard on the shelf. Recipes are by nature derivative and meant to \
 be shared - that is how they improve, are changed, how new ideas are formed.";
 
+// Author's name and url website.
 const authorName = "Jose Ramon";
 const authorUrl = "https://jraleman.com/"
+
+/*
+** Footer component.
+*/
 
 class Footer extends React.Component {
   constructor(props) {
@@ -26,44 +36,6 @@ class Footer extends React.Component {
           </div>
         </div>
       </footer>
-    );
-  }
-}
-
-class About extends React.Component {
-  constructor(props) {
-    super(props);
-    return ;
-  }
-  render() {
-    return (
-      <div className="col-md-6 col-sm-12 text-center">
-        <h3>About</h3>
-        <div className="footer-about">
-          { this.props.info }
-        </div>
-      </div>
-    );
-  }
-}
-
-class Author extends React.Component {
-  constructor(props) {
-    super(props);
-    return ;
-  }
-  render() {
-    return (
-      <div className="col-md-6 col-sm-12 text-center">
-        <div className="footer-author">
-          {"Made with "}
-          <i className="fa fa-heart" aria-hidden="true" />
-          {" love by "}
-          <a href={ this.props.url } target="_blank" rel="noopener noreferrer">
-            { this.props.name } {"."}
-          </a>
-        </div>
-      </div>
     );
   }
 }
