89933f04b445a61a3bde2334ae7ce2dabe9f7ec4
jraleman <jraleman@bendiburg.com>
2018-03-19 00:06:01 -0700

    Refactored Item component

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,15 +1,26 @@
 import React from "react";
 
+/*
+** Item component.
+*/
+
 class Item extends React.Component {
+  constructor(props) {
+    super(props);
+    this.img = this.props.img;
+    this.name = this.props.name;
+    this.url = this.props.url;
+    return ;
+  }
   render() {
     return (
       <div className="col-lg-4 col-md-6 col-sm-12 wow fadeIn">
         <div className="recipe-item text-center">
-          <a href="recipe.html">
-            <img src={ this.props.img } alt={ this.props.name } />
+          <a href={ this.url }>
+            <img src={ this.img } alt={ this.name } />
           </a>
           <br />
-          <h3>{ this.props.name }</h3>
+          <h3>{ this.name }</h3>
         </div>
       </div>
     );
