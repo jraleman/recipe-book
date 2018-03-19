c5267bb654c07244d7133f2d614a0cf65ace24aa
jraleman <jraleman@bendiburg.com>
2018-03-19 00:11:19 -0700

    Refactored RecipeImage component

--- src/components/RecipeImage/RecipeImage.js
+++ src/components/RecipeImage/RecipeImage.js
@@ -1,12 +1,22 @@
 import React from "react";
 
+/*
+** RecipeImage component.
+*/
+
 class RecipeImage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.img = this.props.img;
+    this.name = this.props.name;
+    return ;
+  }
   render() {
     return (
       <div className="col-md-8 pull-left wow swing">
         <img
-          src={ this.props.img }
-          alt={ this.props.name }
+          src={ this.img }
+          alt={ this.name }
           className="recipe-picture"
         />
       </div>
