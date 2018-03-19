b66d0250f892e835d40161ef7ebd63272c972cc7
jraleman <jraleman@bendiburg.com>
2018-03-19 00:13:23 -0700

    Refactored Title component

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,11 +1,20 @@
 import React from "react";
 
+/*
+** Title component.
+*/
+
 class Title extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = this.props.name;
+    return ;
+  }
   render() {
     return (
       <div className="row">
         <div className="col-12">
-          <h2>{ this.props.name }</h2>
+          <h2>{ this.name }</h2>
         </div>
       </div>
     );
