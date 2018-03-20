f856a5b964a452d273513096b992ab1245375817
jraleman <jraleman@bendiburg.com>
2018-03-20 16:22:17 -0700

    Changed Hours to Hrs in InfoTime

--- src/components/RecipeInfo/InfoTime/InfoTime.js
+++ src/components/RecipeInfo/InfoTime/InfoTime.js
@@ -17,7 +17,7 @@ class InfoTime extends React.Component {
           <i className="fa fa-clock-o" aria-hidden="true" />
         </div>
         <div className="col-6">Time</div>
-        <div className="col-4">{ this.time } Hours</div>
+        <div className="col-4">{ this.time } Hrs</div>
       </div>
     );
   }
