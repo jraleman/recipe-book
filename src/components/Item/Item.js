33022a9587d63a792895d830c3f0e94ac7a69cc0
jraleman <jraleman@bendiburg.com>
2018-03-21 00:23:38 -0700

    Refactored Item component

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 
 /*
 ** Item component.
@@ -10,21 +10,13 @@ class Item extends React.Component {
     this.recipe = this.props.recipe;
     this.img = this.recipe.recipeImage;
     this.name = this.recipe.recipeName;
-    this.time = this.recipe.infoTime;
-    this.difficulty = this.recipe.infoDifficulty;
-    this.servings = this.recipe.infoServings;
-    this.ingredients = this.recipe.ingredientsList;
-    this.directions = this.recipe.directionsList;
-    //this.url = <path-to-recipe> - via Link or something...;
     return ;
   }
   render() {
     return (
       <div className="col-lg-4 col-md-6 col-sm-12 wow fadeIn">
         <div className="recipe-item text-center">
-          <a href={ this.url }>
-            <img src={ require("../../assets/" + this.img) } alt={ this.name } />
-          </a>
+          <img src={ require("../../assets/" + this.img) } alt={ this.name } />
           <br />
           <h3>{ this.name }</h3>
         </div>
